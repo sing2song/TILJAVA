a361b04d76d22e023661e5064bb20aff32364744
ssong <sing2song@naver.com>
2021-02-02 12:30:51 +0900

    Update JAVA02.md

---
+++ workspace/Day02/src/operator/add/add.java
@@ -0,0 +1,7 @@
+package operator.add;
+
+public class add {
+	public double add_cal(double a, double b) {
+		return a+b;		
+	}
+}
---
+++ workspace/Day02/src/operator/div/div.java
@@ -0,0 +1,7 @@
+package operator.div;
+
+public class div {
+	public double div_cal(double a, double b){
+		return a/b;
+	}
+}
